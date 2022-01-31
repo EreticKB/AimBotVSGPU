8bcea36b050bc84ff914922ff083450703128f78
EreticKB <59476878+EreticKB@users.noreply.github.com>
2022-01-31 14:49:03 +0300

    1.3.1

--- Assets/_Project/Scripts/UI Scripts/Fly Interface/AdsButtonHandler.cs
+++ Assets/_Project/Scripts/UI Scripts/Fly Interface/AdsButtonHandler.cs
@@ -4,8 +4,8 @@ using UnityEngine.UI;
 
 public class AdsButtonHandler : MonoBehaviour
 {
-    public readonly string _adUnitId = "ca-app-pub-9525013647787018/5102012234";//это реальный код рекламы
-    //public readonly string _adUnitId = "ca-app-pub-3940256099942544/5224354917"; //это код тестовой рекламы, держу чтобы можно было просто менять.
+    //public readonly string _adUnitId = "ca-app-pub-9525013647787018/5102012234";//это реальный код рекламы
+    public readonly string _adUnitId = "ca-app-pub-3940256099942544/5224354917"; //это код тестовой рекламы, держу чтобы можно было просто менять.
     [SerializeField] Button button;
     private void OnEnable()
     {
